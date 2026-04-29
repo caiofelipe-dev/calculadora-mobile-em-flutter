37b30230d086cb6cf77e308aaa3cf96916cfc80f
Caio Felipe <caiofelipesmoreira@gmail.com>
2026-04-29 18:59:22 -0300

    Implementando método que concatena dígito

--- lib/models/memory.dart
+++ lib/models/memory.dart
@@ -1,11 +1,12 @@
 class Memory {
+  /// Lista de operadores aceitos pela calculadora
   static const operations = const ['%', '/', 'x', '-', '+', '='];
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
   late String operation;
-  String _value = '0';
-  bool _wipeValue = false;
+  String _value = '0'; // Valor que está sendo digitado no campo
+  bool _wipeValue = false; // indica se o campo está limpo
 
   void applyCommand(String command) {
     if(command != 'AC') {
@@ -20,21 +21,25 @@ class Memory {
   }
 
   _setOperation(String newOperation) {
-    _wipeValue = true;
+    _wipeValue = true; // indica que o campo está limpo pronto para receber um novo
   }
 
+  /// Atribui o caractere digitado
   _addDigit(String digit) {
-    final isDot = digit == ',';
-    final wipeValue = (_value == '0' && !isDot) || _wipeValue; //verifica se o valor veio depois de uma operação ou se é um número menor que 1
+    final isDot = digit == ','; // indica se o dígito é uma vírgula ou não
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue; // indica se o campo está limpo ou não
 
     if(isDot && _value.contains(',') && !wipeValue) {
-      return; // não permite digitar vírgula mais de uma vez
+      return; // impede que o usuário digite mais de uma vírgula
     }
 
     final emptyValue = isDot ? '0' : '';
-    final currentValue = wipeValue ? emptyValue : _value;
+    final currentValue = wipeValue ? emptyValue : _value; // campo atual (se estiver vazio usa empryValue)
+    
     _value = currentValue + digit;
-    _wipeValue = false;
+    _wipeValue = false; // indica que o campo não está limpo
+    /// armazena o valor no buffer
+    _buffer[_bufferIndex] = double.tryParse(_value.replaceAll('.', '').replaceAll(',', '.')) ?? 0;
   }
 
   _allClear() {
